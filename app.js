b387dc7097266ddf70ccf9f16a476b5392511345
rukchanok krumsamer <pepoil07@hotmail.com>
2023-02-21 19:40:31 +0700

    Add project 21 feb 2023
    To add project

---
+++ app.js
@@ -0,0 +1,5 @@
+var screenWidth = window.screen.width;
+var screenHeight = window.screen.height;
+
+console.log("Screen width: " + screenWidth + " pixels");
+console.log("Screen height: " + screenHeight + " pixels");
